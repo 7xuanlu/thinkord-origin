d15ff8a48a3d7d7ed801a7cf77af0f54a2a20633
Mnemo <h164654156465@gmail.com>
2019-05-05 18:12:16 +0800

    create screenshot with uuid and enable win10 notification if file is created

--- renderer-process/media/desktop-capturer.js
+++ renderer-process/media/desktop-capturer.js
@@ -2,9 +2,10 @@ const electron = require('electron');
 const {desktopCapturer, screen, shell} = electron;
 const remote = require('electron').remote;
 const app = remote.app;
-const fs = require('fs')
-const os = require('os')
-const path = require('path')
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+const uuidv1 = require('uuid/v1');
 
 const screenshot = document.getElementById('screenshot')
 // const screenshotMsg = document.getElementById('screenshot-path')
@@ -21,11 +22,31 @@ screenshot.addEventListener('click', (event) => {
 
     sources.forEach((source) => {
       if (source.name === 'Entire screen' || source.name === 'Screen 1') {
-        const screenshotPath = path.join(userPath, 'screenshot.png');
-        console.log(screenshotPath);
+        const screenshotName = `${uuidv1()}.png`;
+        let screenshotPath = path.join(userPath, screenshotName);
+
+        // fs.access(`${screenshotPath}`, (err) => {
+        //   if (err) {
+        //     screenshotPath = path.join(userPath, 'screenshot1.png');
+        //   } else {
+        //     screenshotPath.replace(`${num}`, `${num+1}`)
+        //     num++;
+        //   }
+        // })
 
         fs.writeFile(screenshotPath, source.thumbnail.toPNG(), (error) => {
-          if (error) return console.log(error);
+          if (error) {
+            return console.log(error);
+          } else {
+            let myNotification = new Notification(
+              '已經幫您存好檔案囉!', {
+                  body: `檔案路徑 ${screenshotPath}`
+              });
+            
+            myNotification.onclick = () => {
+              console.log('Notification clicked')
+            }
+          }
           //shell.openExternal(`file://${screenshotPath}`);
 
           //const message = `Saved screenshot to: ${screenshotPath}`
