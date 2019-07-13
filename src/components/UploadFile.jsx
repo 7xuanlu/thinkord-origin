bd2ee5176485d6df39d031f3fe6af792aaad805d
Tim <a0987642936@gmail.com>
2019-07-13 13:37:58 +0800

    Uploading file_ver4(timeline can open multiple file respectively,and cannot be overwitten)

--- src/components/UploadFile.jsx
+++ src/components/UploadFile.jsx
@@ -1,9 +1,9 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Dropzone from 'react-dropzone';
-import {Button} from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import styled from 'styled-components';
 import Image from 'react-image-resizer';
-import {shell} from 'electron';
+import { shell } from 'electron';
 
 class Uploadfile extends Component {
   constructor(props) {
@@ -18,27 +18,27 @@ class Uploadfile extends Component {
   }
 
   onDrop(files) {
+    let filepaths = []
+    files.map((file) => {
+      filepaths.push(file.path)
+    })
     this.setState({
-      files: files.map(file => Object.assign(file, {
-        preview: URL.createObjectURL(file)
+      files: this.state.files.concat(files.map(file => Object.assign(file, {
+        preview: filepaths
 
-      }))
+      })))
     })
   }
   //for files that are not images.
-  OpenFileButton(e){
-    const fileBtn = document.getElementById('fileBtn');
-    if(fileBtn){
-        console.log("catch you")
-        shell.openItem("路徑")
-    }
+  OpenFileButton(e, filepath) {
+    console.log(e, filepath)
+    shell.openItem(e, filepath)
   }
 
+  render() {
 
-  render(){
-    
     const getColor = (props) => {
-      return '#00e676';
+      return '#eeeeee';
     }
     const Container = styled.div`
       flex: 1;
@@ -59,32 +59,32 @@ class Uploadfile extends Component {
     // const imagePreviewUrl = this.state;
     const files = this.state.files.map(file => {
       const fileType = file['type'];
-      let imageTypes = ['image/gif','image/jpeg','image/png'];
-      if(imageTypes.includes(fileType)){
-        return(
+      let imageTypes = ['image/gif', 'image/jpeg', 'image/png'];
+      if (imageTypes.includes(fileType)) {
+        return (
           <div>
             <li key={file.name}>
               {file.name}
-              <br/>
-              <Image 
-                src = {file.preview}
+              <br />
+              <Image
+                src={file.path}
                 width={600}
                 height={400}
               />
             </li>
           </div>
         )
-      }else{
-        return(
+      } else {
+        return (
           <div>
             <li key={file.name}>
               {file.name}
-              <br/>
-            <Button
-              id="fileBtn"
-              size='sm'
-              onClick={this.OpenFileButton}
-            >open!</Button>  
+              <br />
+              <Button
+                id="fileBtn"
+                size='sm'
+                onClick={this.OpenFileButton.bind(this, file.path)}
+              >open!</Button>
             </li>
           </div>
         )
@@ -93,16 +93,16 @@ class Uploadfile extends Component {
 
     return (
       <Dropzone onDrop={this.onDrop.bind(this)}>
-        {({getRootProps, getInputProps}) => (
+        {({ getRootProps, getInputProps, isDragActive }) => (
           <div className="container">
-            <Container {...getRootProps({className: 'dropzone'})}>
+            <Container {...getRootProps({ className: 'dropzone' })}>
               <input {...getInputProps()} />
-              <p>Drag 'n' drop some files here, or click to select files</p>
+              {isDragActive ? 'Drop it!' : 'Drag and drop some files here, or click to select files'}
             </Container>
             <aside>
               <h4>Files</h4>
               {files}
-              
+
             </aside>
           </div>
         )}
