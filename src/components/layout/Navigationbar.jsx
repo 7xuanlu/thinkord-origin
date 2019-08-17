603d9f47cc6963f1817e1dc58d82680c73cb6be3
NienZL <nien999@g.ncu.edu.tw>
2019-08-17 15:09:35 +0800

    adjust Navbar

--- src/components/layout/Navigationbar.jsx
+++ src/components/layout/Navigationbar.jsx
@@ -5,25 +5,38 @@ export class Navigationbar extends Component {
         super(props);
     }
 
-    //select all checkboxes
-    selectAllBoxes = () => {
-        const selectAll = document.getElementsByClassName("check");
+    changeMode = () => {
+        const selectBox = document.getElementsByClassName("checkContainer");
         let i = 0;
-        let alreadySelectAll = true;
-        for (i = 0; i < selectAll.length; i++) {
-            if (selectAll[i].checked != true) { alreadySelectAll = false }
-        }
 
-        if (alreadySelectAll) {
-            for (i = 0; i < selectAll.length; i++) {
-                selectAll[i].checked = false;
-            }
+        if (document.getElementsByClassName("viewMode")[0].style.display === "flex") {
+            document.getElementsByClassName("viewMode")[0].style.display = "none";         //show selectMode
+            document.getElementsByClassName("selectMode")[0].style.display = "flex";
+            for (i = 0; i < selectBox.length; i++) { selectBox[i].style.display = "block"; }
         } else {
-            for (i = 0; i < selectAll.length; i++) {
-                selectAll[i].checked = true;
-            }
+            document.getElementsByClassName("selectMode")[0].style.display = "none";        //show ViewMode
+            document.getElementsByClassName("viewMode")[0].style.display = "flex";
+            for (i = 0; i < selectBox.length; i++) { selectBox[i].style.display = "none"; }
         }
     }
+
+    // //select all checkboxes
+    // selectAllBoxes = () => {
+    //     const selectAll = document.getElementsByClassName("check");
+    //     let i = 0;
+    //     let alreadySelectAll = true;
+    //     for (i = 0; i < selectAll.length; i++) {
+    //         if (selectAll[i].checked != true) { alreadySelectAll = false }
+    //     }
+
+    //     if (alreadySelectAll) {
+    //         for (i = 0; i < selectAll.length; i++) 
+    //             selectAll[i].checked = false;
+    //     } else {
+    //         for (i = 0; i < selectAll.length; i++) 
+    //             selectAll[i].checked = true;
+    //     }
+    // }
 
 
     render() {
@@ -31,13 +44,22 @@ export class Navigationbar extends Component {
             <div className="navigatorContainer" >
                 <div className="navigationBar viewMode">
                     <div className="search"><div><input type="text" placeholder=" Search . . ." required /></div></div>
-                    <i className="fas fa-undo" onClick={this.props.clickPreviousStep}></i>
-                    <i className="fas fa-redo" onClick={this.props.clickNextStep}></i>
-                    <i className="far fa-check-square" onClick={this.selectAllBoxes}></i>
+                    <i className="fas fa-undo-alt" onClick={this.props.clickPreviousStep}></i>
+                    <i className="fas fa-redo-alt" onClick={this.props.clickNextStep}></i>
                     <i className="far fa-save" onClick={this.props.clickSave}></i>
+                    <i className="far fa-square" onClick={this.changeMode}></i>
+                    <i className="fas fa-angle-double-up"></i>
                     <i className="fas fa-angle-up" onClick={this.props.clickTop}></i>
                     <i className="fas fa-angle-down" onClick={this.props.clickBottom}></i>
                     <i className="fas fa-home" onClick={this.props.clickHome}></i>
+                </div>
+                <div className="navigationBar selectMode">
+                    <div className="search"><div><input type="text" placeholder=" Search . . ." required /></div></div>
+                    <i className="fas fa-arrow-left" onClick={this.changeMode}></i>
+                    <i className="far fa-check-square" ></i>
+                    <i className="fas fa-trash"></i>
+                    <i className="fas fa-bookmark"></i>
+                    <i className="fas fa-angle-double-up"></i>
                 </div>
             </div>
         )
