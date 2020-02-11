5d91a02842947631dc1bb4976c8488eb8add2de9
h164654156465 <h164654156465@gmail.com>
2020-02-11 14:15:26 +0800

    Update json-manager.js comment

--- src/renderer/json-manager.js
+++ src/renderer/json-manager.js
@@ -19,7 +19,7 @@ export class JSONManager {
             "name": ""
         }
 
-        // slu's data format
+        // Timeline's data format
         this.initSluObj = {
             "id": sluId,
             "name": "",
@@ -27,13 +27,14 @@ export class JSONManager {
         }
     }
 
+    // Initialize a new timeline in app.json and directory 'Slu'
     async initJSON() {
         let counter = 1;
-        let userDir = app.getPath('userData');
+        let userDir = app.getPath('userData');  // User's directory.
         let appSettingPath = path.join(userDir, 'app.json');
-        let sluDir = path.join(userDir, 'Slu');
-        let sluPath = path.join(sluDir, 'Untitled 1.json');
-        let sluName = "Untitled 1";
+        let sluDir = path.join(userDir, 'Slu');  // Timeline's directory.
+        let sluPath = path.join(sluDir, 'Untitled 1.json');  // Default timeline's path.
+        let sluName = "Untitled 1";  // Default timeline's name.
 
         // If the file "Untitled 1" is existed, it will be incremented by 1
         while (fs.existsSync(sluPath)) {
@@ -42,108 +43,101 @@ export class JSONManager {
             sluPath = path.join(sluDir, "Untitled " + counter + ".json");
         }
 
-        this.initSluObj.name = sluName;
+        this.initSluObj.name = sluName;  // Update timeline's name.
 
-        let jsonString = JSON.stringify(this.initSluObj);
+        let jsonString = JSON.stringify(this.initSluObj); // Convert JS Object to string.
 
+        // Write the string to timeline's file.
         fs.writeFile(sluPath, jsonString, (err) => {
-            if (err) { throw err; }
+            if (err) throw err;
         });
 
-        // Insert newly created slu's path to app.json
+        // Insert newly created timeline's path to app.json
         fs.readFile(appSettingPath, (err, data) => {
-            if (err) {
-                throw err;
-            } else {
-                // Parse string to JS object
-                let json = JSON.parse(data);
+            if (err) throw err;
 
-                // Record the path of user's newly created slu 
-                this.initSluPathObj.path = sluPath;
-                this.initSluPathObj.name = sluName;
-                json['slus'].push(this.initSluPathObj);
+            let json = JSON.parse(data);  // Parse string to JS object
 
-                let jsonString = JSON.stringify(json);
+            // Record the path, which is user's newly created timeline.
+            this.initSluPathObj.path = sluPath;  // Update timeline's path.
+            this.initSluPathObj.name = sluName;  // Update timeline's name.
+            json['slus'].push(this.initSluPathObj);
 
-                fs.writeFile(appSettingPath, jsonString, (err) => {
-                    if (err) { throw err; }
-                });
-            }
+            let jsonString = JSON.stringify(json);  // Convert JS object to string.
+
+            // Write the string to app.json.
+            fs.writeFile(appSettingPath, jsonString, (err) => {
+                if (err) throw err;
+            });
         });
 
         return sluPath;
     }
 
-    // Define a function getting our user's Slu file
+    // Get user's timeline file
     readJSON(sluPath) {
+        // Return a promise object, waiting for it to be resolved.
         return new Promise((resolve, reject) => {
+            // Check whether the file is existed.
             fs.access(sluPath, (err) => {
-                if (err) {
-                    throw err;
-                } else {
-                    console.log('File existed, trying to read slu json file')
-                    fs.readFile(sluPath, (err, data) => {
-                        if (err) {
-                            throw err;
-                        }
-                        // Parse json to JS object
-                        let json = JSON.parse(data);
-                        console.log(json);
-                        resolve(json);
-                    });
-                }
-            })
-        })
-    }
+                if (err) throw err;
 
-    async writeJSON(json, sluPath) {
-        let jsonString = JSON.stringify(json);
+                console.log('File existed, trying to read slu json file')
+                fs.readFile(sluPath, (err, data) => {
+                    if (err) throw err
 
-        // Write to the original json file
-        fs.writeFile(sluPath, jsonString, 'utf8', (err) => {
-            if (err) { throw err; }
+                    let json = JSON.parse(data);  // Parse json to JS object
+
+                    resolve(json);
+                });
+            });
         });
     }
 
-    // Rename slu in app.json
+    // Write data to user's timeline file.
+    async writeJSON(json, sluPath) {
+        let jsonString = JSON.stringify(json);  // Convert JS object to string.
+
+        // Write the string to the original timeline file
+        fs.writeFile(sluPath, jsonString, 'utf8', (err) => {
+            if (err) throw err;
+        });
+    }
+
+    // Rename timeline in app.json
     renameSluAppJSON(sluId, newSluName) {
         const newSluPath = path.join(sluDirPath, newSluName + '.json');
         // const newSluName = args.newSluName;
         let oldSluName = null;
 
         fs.readFile(appSettingPath, (err, data) => {
-            if (err) {
-                throw err;
-            } else {
-                // Parse string to JS object
-                let json = JSON.parse(data);
+            if (err) throw err;
 
-                json["slus"].map((item, index) => {
-                    if (item["id"] === sluId) {
-                        oldSluName = json["slus"][index].name;
-                        json["slus"][index].path = newSluPath;
-                        json["slus"][index].name = newSluName;
-                    }
-                });
+            let json = JSON.parse(data);  // Parse string to JS object
 
-                let jsonString = JSON.stringify(json);
+            // Loop through array.
+            json["slus"].map((item, index) => {
+                if (item["id"] === sluId) {
+                    oldSluName = json["slus"][index].name;
+                    json["slus"][index].path = newSluPath;  // Update timeline's path.
+                    json["slus"][index].name = newSluName;  // Update timeline's name.
+                }
+            });
 
-                fs.writeFile(appSettingPath, jsonString, (err) => {
-                    if (err) { throw err }
-                });
-            }
+            let jsonString = JSON.stringify(json);  // Convert JS object to string.
+
+            fs.writeFile(appSettingPath, jsonString, (err) => {
+                if (err) throw err;
+            });
         });
     }
 
+    // Rename timeline's name in file system.
     renameSluFile(sluPath, newSluName) {
         const newSluPath = path.join(sluDirPath, newSluName + '.json');
-        
+
         fs.rename(sluPath, newSluPath, (err) => {
-            if (err) {
-                throw err
-            } else {
-                
-            }
+            if (err) throw err;
         });
     }
 }
