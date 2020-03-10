74446baccdc6dc9b2c06a14b69fb66351055e5a9
MingHao Juan <howardjuan0118@gmail.com>
2020-03-10 16:29:59 +0800

    Fix bug

--- test/spec.js
+++ test/spec.js
@@ -3,10 +3,10 @@ const assert = require('assert');
 const electronPath = require('electron');
 const path = require('path');
 
-describe('Application launch', () => {
+describe('Application launch', function () {
   this.timeout(100000);
 
-  beforeEach(() => {
+  beforeEach(function () {
     this.app = new Application({
       path: electronPath,
       args: [path.join(__dirname, '..')]
@@ -14,14 +14,14 @@ describe('Application launch', () => {
     return this.app.start();
   });
 
-  afterEach(() => {
+  afterEach(function () {
     if (this.app && this.app.isRunning()) {
       return this.app.stop();
     }
   });
 
-  it('shows an initial window', () => {
-    return this.app.client.getWindowCount().then((count) => {
+  it('shows an initial window', function () {
+    return this.app.client.getWindowCount().then(function (count) {
       assert.equal(count, 1);
       // Please note that getWindowCount() will return 2 if `dev tools` are opened.
     });
