daa6209fac86b16250f82c35ee2872eb041bb3eb
timlo <a0987642936@yahoo.com>
2020-03-13 16:59:58 +0800

    Fix the fail in main.js (require nonexisting file)

--- app/models/index.js
+++ app/models/index.js
@@ -7,7 +7,7 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
-
+  
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
